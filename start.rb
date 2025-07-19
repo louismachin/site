33906b0ac73312c7b3a9b9cdf2e55b637e3f33e6
louis <louis@machin.dev>
2025-07-19 23:11:31 +0100

    Updated configuration

--- start.rb
+++ start.rb
@@ -5,9 +5,13 @@ require_relative './models/environment'
 
 APP_ROOT = File.expand_path(__dir__)
 
-set :bind, '0.0.0.0'
-set :port, $env.port
-set :public_folder, File.expand_path('public', __dir__)
+configure do
+    set :bind, '0.0.0.0'
+    set :port, $env.port
+    set :public_folder, File.expand_path('public', __dir__)
+    set :environment, :production
+    disable :protection
+end
 
 require_relative './helpers/markdown'
 require_relative './helpers/content'
